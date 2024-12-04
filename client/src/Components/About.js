5f2121686c9d752cbeccc7e8f0fe29445b389e2c
alsh7i.130@gmail.com <Student@SH-A148-03.utas.edu.om>
2024-12-04 14:38:32 +0400

    2nd update on update.js dept.js

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div>
-      <h3>Anthony - 66J1234 - 1st update</h3>
+      <h3>Anthony - 66J1234 - 2nd update</h3>
     </div>
   );
 };
--- client/src/Components/Dept.js
+++ client/src/Components/Dept.js
@@ -1,5 +1,5 @@
 const Departments = () => {
-  return <div>Departments</div>;
+  return <div>Departments - 1st update</div>;
 };
 
 export default Departments;
